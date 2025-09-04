e5d823d1b6458706dc28573f9ccff03d1c4f08eb
gen1nh <genisson465@gmail.com>
2025-09-04 16:03:18 -0300

    animation menu

--- src/js/script.js
+++ src/js/script.js
@@ -36,3 +36,21 @@ const swiper3 = new Swiper(".mySwiper3", {
     pauseOnMouseEnter: true,
   },
 });
+
+const menuToggleIcon = document.getElementById('menu-toggle-icon');
+const menuSair = document.getElementById('menu-sair');
+
+let isOpen = false;
+
+menuToggleIcon.addEventListener('click', () => {
+
+  menuSair.classList.toggle('hidden');
+
+  isOpen = !isOpen;
+
+  if (isOpen) {
+    menuToggleIcon.src = "src/assets/close.svg"; // X
+  } else {
+    menuToggleIcon.src = "src/assets/link.svg"; // hambúrguer
+  }
+});
