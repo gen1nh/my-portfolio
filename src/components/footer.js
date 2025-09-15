fc452f82173f60c828aa4161770683c2adef50b8
gen1nh <genisson465@gmail.com>
2025-09-15 13:27:07 -0300

    Hotfix: ajustando bugs

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,12 @@ export function Footer() {
       <div class="flex flex-row gap-4 justify-center">
         <a href="https://github.com/gen1nh"><img src="src/assets/footer/F-GitHub.svg" alt="GitHub"/></a>
         <a href="https://dribbble.com/gen1nh"><img src="src/assets/footer/F-Dribbble.svg" alt="Dribbble"/></a>
-        <a href="https://instagram.com/gen1nh"><img src="src/assets/footer/F-Instagram.svg" alt="Instagram"/></a>
-        <a href="https://linkedin.com/in/gen1nh"><img src="src/assets/footer/F-Linkedin.svg" alt="Linkedin"/></a>
+        <a href="https://instagram.com/gen1nh">
+            <img src="src/assets/footer/F-Instagram.svg" alt="Instagram"/>
+        </a>
+        <a href="https://linkedin.com/in/gen1nh">
+           <img src="src/assets/footer/opa.svg" alt="Linkedin"/>
+        </a>
       </div>
       <p class="uppercase font-secondary">Copyright © 2025 gen1nh</p>
     </footer>
