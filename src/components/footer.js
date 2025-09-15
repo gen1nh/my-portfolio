fec4cca60b6bdc9b0cf141a5b618af18855179f9
gen1nh <genisson465@gmail.com>
2025-09-15 13:40:23 -0300

    Hotfix: ajustando bug nas imagens das skills

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,13 @@ export function Footer() {
   return `
     <footer class="flex flex-col gap-4 mt-[128px] text-center mb-[32px]">
       <div class="flex flex-row gap-4 justify-center">
-        <a href="https://github.com/gen1nh"><img src="src/assets/footer/F-GitHub.svg" alt="GitHub"/></a>
-        <a href="https://dribbble.com/gen1nh"><img src="src/assets/footer/F-Dribbble.svg" alt="Dribbble"/></a>
-        <a href="https://instagram.com/gen1nh">
-            <img src="src/assets/footer/F-Instagram.svg" alt="Instagram"/>
+        <a href="https://github.com/gen1nh" target="_blank"><img src="src/assets/footer/gitHub.svg" alt="GitHub"/></a>
+        <a href="https://dribbble.com/gen1nh" target="_blank"><img src="src/assets/footer/dribbble.svg" alt="Dribbble"/></a>
+        <a href="https://instagram.com/gen1nh" target="_blank">
+            <img src="src/assets/footer/instagram.svg" alt="Instagram"/>
         </a>
-        <a href="https://linkedin.com/in/gen1nh">
-           <img src="src/assets/footer/opa.svg" alt="Linkedin"/>
+        <a href="https://linkedin.com/in/gen1nh" target="_blank"></a>
+           <img src="src/assets/footer/linkedin.svg" alt="Linkedin"/>
         </a>
       </div>
       <p class="uppercase font-secondary">Copyright © 2025 gen1nh</p>
